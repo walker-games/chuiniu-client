02f6f7b0f0759f0aa077fac6f60e54574fdd9bd2
walker <zw37520@gmail.com>
2026-04-09 18:09:10 +0800

    feat: project scaffold with Vue 3, Vite, Tailwind, Vant

---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center">
+    <h1 class="text-4xl font-bold" style="color: #D4A853; font-family: 'Ma Shan Zheng', serif;">吹牛骰子</h1>
+  </div>
+</template>
